78bc11cac6c9513bf4724c8a690cfdbf7d06c774
ryanido <74929384+ryanido@users.noreply.github.com>
2023-02-20 21:54:50 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { Suspense, useRef, useState } from 'react'
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, useGLTF } from '@react-three/drei';
+import { Model } from './Shoe';
+
 
 function App() {
+  const ref = useRef()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="card">
+        <div className="product-canvas">
+          <Canvas>
+            <Suspense fallback={null}>
+              <ambientLight />
+              <spotLight intensity={0.9}
+                angle={0.1}
+                penumbra={1}
+                position={[10, 15, 10]} castShadow />
+              <Model />
+              <OrbitControls 
+              enablePan={true} 
+              enableZoom={true}
+              enableRotate={true}/>
+            </Suspense>
+          </Canvas>
+        </div>
+        <h2>Color chooser</h2>
+        <div className='colors'>
+          <div>
+            <input type="color" id="head" name="head"
+              value="#e66465" />
+            <label for="head">Main</label>
+          </div>
+
+          <div>
+            <input type="color" id="body" name="body"
+              value="#f6b73c" />
+            <label for="body">Stripes</label>
+          </div>
+          <div>
+            <input type="color" id="body" name="body"
+              value="#f6b73c" />
+            <label for="body">Soul</label>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
