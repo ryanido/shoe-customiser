d932e025cdcfe7eecae49cc84f521a5cedbb6038
ryanido <74929384+ryanido@users.noreply.github.com>
2023-02-24 17:56:20 +0000

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,54 +1,94 @@
 import logo from './logo.svg';
 import './App.css';
+import './fonts.css'
 import { Suspense, useRef, useState } from 'react'
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei';
 import { Model } from './Shoe';
+import styled from 'styled-components';
 
+
+function ColorButton({ color, setColor }) {
+  return (
+    <button style={{
+      backgroundColor: color,
+      borderRadius: 30, 
+      width: 30, 
+      aspectRatio: 1
+    }} onClick={() => setColor(color)} />
+  );
+}
 
 function App() {
   const ref = useRef()
+  const [lacesColor, setLacesColor] = useState('white');
+  const [meshColor, setMeshColor] = useState('blue');
+  const [capsColor, setCapsColor] = useState('black');
+  const [innerColor, setInnerColor] = useState('blue');
+  const [soleColor, setSoleColor] = useState('blue');
+  const [stripesColor, setStripesColor] = useState('white');
+  const [bandColor, setBandColor] = useState('black');
+  const [patchColor, setPatchColor] = useState('blue');
+  const shoe = {
+    "laces": lacesColor,
+    "mesh": meshColor,
+    "caps": capsColor,
+    "inner": innerColor,
+    "sole": soleColor,
+    "stripes": stripesColor,
+    "band": bandColor,
+    "patch": patchColor
+  }
   return (
-    <div className="wrapper">
-      <div className="card">
-        <div className="product-canvas">
-          <Canvas>
-            <Suspense fallback={null}>
-              <ambientLight />
-              <spotLight intensity={0.9}
-                angle={0.1}
-                penumbra={1}
-                position={[10, 15, 10]} castShadow />
-              <Model />
-              <OrbitControls 
-              enablePan={true} 
+    <Wrapper>
+      <ProductCanvas>
+        <Canvas>
+          <Suspense fallback={null}>
+            <ambientLight />
+            <spotLight intensity={0.9}
+              angle={0.1}
+              penumbra={1}
+              position={[10, 15, 10]} castShadow />
+            <Model shoe={shoe} />
+            <OrbitControls
+              enablePan={true}
               enableZoom={true}
-              enableRotate={true}/>
-            </Suspense>
-          </Canvas>
-        </div>
-        <h2>Color chooser</h2>
-        <div className='colors'>
-          <div>
-            <input type="color" id="head" name="head"
-              value="#e66465" />
-            <label for="head">Main</label>
-          </div>
-
-          <div>
-            <input type="color" id="body" name="body"
-              value="#f6b73c" />
-            <label for="body">Stripes</label>
-          </div>
-          <div>
-            <input type="color" id="body" name="body"
-              value="#f6b73c" />
-            <label for="body">Soul</label>
-          </div>
-        </div>
-      </div>
-    </div>
+              enableRotate={true} />
+          </Suspense>
+        </Canvas>
+      </ProductCanvas>
+      <Customization>
+        <h1> Laces </h1>
+        <CustomizationOptions>
+          <ColorButton color={'red'} setColor={setLacesColor} />
+        </CustomizationOptions>
+      </Customization>
+    </Wrapper>
   );
 }
+
+const ProductCanvas = styled.div`
+  width: 100vw;
+  height: 70vh;
+  background:whitesmoke;
+`
+
+const Wrapper = styled.div`
+  min-height: 100vh;
+  align-items: center;
+  justify-content: center;
+  font-family:Montseratt;
+`
+const Customization = styled.div` 
+  text-align: center;
+  padding: 10px;
+`
+const CustomizationOptions = styled.div`
+  display:flex;
+  flex-direction:row;
+  justify-content: center;
+  gap: 100px;
+`
+
 
 export default App;
--- src/Shoe.jsx
+++ src/Shoe.jsx
@@ -6,19 +6,19 @@ Command: npx gltfjsx@6.1.4 shoe.gltf
 import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 
-export function Model(props) {
+export function Model({shoe}) {
   const group = useRef()
   const { nodes, materials } = useGLTF('/shoe.gltf')
   return (
-    <group ref= {group}{...props} dispose={null} scale = {3}>
-      <mesh geometry={nodes.shoe.geometry} material={materials.laces} material-color ={"red"}/>
-      <mesh geometry={nodes.shoe_1.geometry} material={materials.mesh}  material-color = {"blue"}/>
-      <mesh geometry={nodes.shoe_2.geometry} material={materials.caps} material-color = {"blue"} />
-      <mesh geometry={nodes.shoe_3.geometry} material={materials.inner} material-color = {"blue"} />
-      <mesh geometry={nodes.shoe_4.geometry} material={materials.sole} material-color = {"blue"} />
-      <mesh geometry={nodes.shoe_5.geometry} material={materials.stripes} material-color = {"blue"} />
-      <mesh geometry={nodes.shoe_6.geometry} material={materials.band} material-color = {"blue"} />
-      <mesh geometry={nodes.shoe_7.geometry} material={materials.patch} material-color = {"blue"} />
+    <group ref= {group} dispose={null} scale = {3}>
+      <mesh geometry={nodes.shoe.geometry} material={materials.laces} material-color ={shoe.laces}/>
+      <mesh geometry={nodes.shoe_1.geometry} material={materials.mesh}  material-color = {shoe.mesh}/>
+      <mesh geometry={nodes.shoe_2.geometry} material={materials.caps} material-color = {shoe.caps} />
+      <mesh geometry={nodes.shoe_3.geometry} material={materials.inner} material-color = {shoe.inner} />
+      <mesh geometry={nodes.shoe_4.geometry} material={materials.sole} material-color = {shoe.sole} />
+      <mesh geometry={nodes.shoe_5.geometry} material={materials.stripes} material-color = {shoe.stripes} />
+      <mesh geometry={nodes.shoe_6.geometry} material={materials.band} material-color = {shoe.band} />
+      <mesh geometry={nodes.shoe_7.geometry} material={materials.patch} material-color = {shoe.patch} />
     </group>
   )
 }
